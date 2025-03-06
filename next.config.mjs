2cb19a61e08d819f2051ff296d4d24c263e6abf6
bijendra <bizen.manandhar@gmail.com>
2025-03-06 13:52:50 +0545

    update configs

--- next.config.mjs
+++ next.config.mjs
@@ -4,12 +4,17 @@ try {
 } catch (e) {
   // ignore error
 }
+const isProd = process.env.NODE_ENV === 'production';
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  reactStrictMode:true,
   eslint: {
     ignoreDuringBuilds: true,
   },
+  assetPrefix: isProd ? '/' : '',
+  basePath: isProd ? '/' : '',
+  output: 'export',
   typescript: {
     ignoreBuildErrors: true,
   },
@@ -20,6 +25,39 @@ const nextConfig = {
     webpackBuildWorker: true,
     parallelServerBuildTraces: true,
     parallelServerCompiles: true,
+  },
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'Strict-Transport-Security',
+            value: 'max-age=63072000; includeSubDomains; preload'
+          },
+          {
+            key: 'Content-Security-Policy',
+            value: "default-src 'self'; script-src 'self' 'unsafe-inline' 'unsafe-eval'; style-src 'self' 'unsafe-inline'; img-src 'self' data: https://hebbkx1anhila5yf.public.blob.vercel-storage.com  https://picsum.photos https://*.picsum.photos/*; font-src 'self'; connect-src 'self' https://simple-email-api-jyzt.onrender.com; frame-src 'self'; object-src 'none'; base-uri 'self'; form-action 'self';" 
+          },
+          {
+            key: 'X-Frame-Options',
+            value: 'SAMEORIGIN'
+          },
+          {
+            key: 'X-Content-Type-Options',
+            value: 'nosniff'
+          },
+          {
+            key: 'Referrer-Policy',
+            value: '*'
+          },
+          {
+            key: 'Permissions-Policy',
+            value: 'geolocation=(), microphone=(), camera=(), payment=()'
+          }
+        ],
+      },
+    ];
   },
 }
 
