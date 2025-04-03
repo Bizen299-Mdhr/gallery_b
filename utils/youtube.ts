626ba5efcddf602ed1fb7eb3e4c9b1be8f54c751
bijendra <bizen.manandhar@gmail.com>
2025-04-03 07:23:05 +0545

    chore: updates key logic

--- utils/youtube.ts
+++ utils/youtube.ts
@@ -1,5 +1,5 @@
 // YouTube API utility functions
-
+console.log(process.env,'process.env');
 const YOUTUBE_API_KEY = process.env.NEXT_PUBLIC_YOUTUBE_API_KEY || ''; // You'll need to add this to your environment variables
 const YOUTUBE_API_KEY_DEV = process.env.YOUTUBE_API_KEY_DEV || ''; // You'll need to add this to your environment variables
 console.log(YOUTUBE_API_KEY , 'YOUTUBE_API_KEY');
